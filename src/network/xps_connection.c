bf74e3016b4d387ad943b3c18098046051ca389d
Vangala Rasvitha <vangalarasvitha@gmail.com>
2026-08-31 18:18:53 +0000

    Stage 7

--- src/network/xps_connection.c
+++ src/network/xps_connection.c
@@ -1,5 +1,10 @@
 #include "../xps.h"
-xps_connection_t *xps_connection_create(int epoll_fd, int sock_fd) {
+
+// Function declaration for read callback of listener
+// Function declaration for read callback of connection
+void connection_loop_read_handler(void *ptr);
+
+xps_connection_t *xps_connection_create(xps_core_t *core, u_int sock_fd) {
 
   xps_connection_t *connection = (xps_connection_t *)malloc(sizeof(xps_connection_t)); 
   if (connection == NULL) {
@@ -8,16 +13,18 @@ xps_connection_t *xps_connection_create(int epoll_fd, int sock_fd) {
   }
 
   /* attach sock_fd to epoll */
-   xps_loop_attach(epoll_fd, sock_fd, EPOLLIN);
-
+  // stage 6
+  // xps_loop_attach(epoll_fd, sock_fd, EPOLLIN);
+  // stage 7 Replace old xps_loop_attach with modified one
+  xps_loop_attach(core->loop, sock_fd, EPOLLIN, connection, connection_loop_read_handler);
   // Init values
-  connection->epoll_fd = epoll_fd;
+  connection->core = core;
   connection->sock_fd = sock_fd;
   connection->listener = NULL;
   connection->remote_ip = get_remote_ip(sock_fd);
 
   /* add connection to 'connections' list */
-  vec_push(&connections, connection);
+  vec_push(&core->connections, connection);
 
   logger(LOG_DEBUG, "xps_connection_create()", "created connection");
   return connection;
@@ -29,19 +36,20 @@ void xps_connection_destroy(xps_connection_t *connection) {
   assert(connection != NULL);
 
   /* set connection to NULL in 'connections' list */
-    for(int i=0; i < connections.length; i++)
+    xps_core_t *core = connection->core;
+    for(int i=0; i < core->connections.length; i++)
     {
-     xps_connection_t *curr = connections.data[i];
+     xps_connection_t *curr = core->connections.data[i];
 
      if(curr == connection)
       {
-        connections.data[i] = NULL;
+        core->connections.data[i] = NULL;
         break;
       }
      }
     
   /* detach connection from loop */
-    xps_loop_detach(connection->epoll_fd, connection->sock_fd);
+    xps_loop_detach(connection->core->loop, connection->sock_fd);
 
   /* close connection socket FD */
     close(connection->sock_fd);
@@ -71,11 +79,12 @@ void strrev(char *str) {
     }
 }
 
-void xps_connection_read_handler(xps_connection_t *connection) {
+void connection_loop_read_handler(void *ptr) {
 
   /* validate params */
-  assert(connection != NULL);
-
+  assert(ptr != NULL);
+  xps_connection_t *connection = ptr;
+  
   char buff[DEFAULT_BUFFER_SIZE];
   long read_n = recv(connection->sock_fd, buff,sizeof(buff)-1, 0);
 
--- src/network/xps_listener.c
+++ src/network/xps_listener.c
@@ -1,6 +1,8 @@
 #include "../xps.h"
 
-xps_listener_t *xps_listener_create(int epoll_fd, const char *host, u_int port) {
+void listener_connection_handler(void *ptr);
+
+xps_listener_t *xps_listener_create(xps_core_t *core, const char *host, u_int port) {
     assert(host != NULL);
     assert(is_valid_port(port)); // Will be explained later
 
@@ -56,16 +58,20 @@ xps_listener_t *xps_listener_create(int epoll_fd, const char *host, u_int port) 
     }
 
     // Init values
-    listener->epoll_fd = epoll_fd;
+    listener->core = core;
     listener->host = host;
     listener->port = port;
     listener->sock_fd = sock_fd;
 
     // Attach listener to loop
-    xps_loop_attach(epoll_fd, sock_fd, EPOLLIN);
+    // stage 6 
+    // xps_loop_attach(epoll_fd, sock_fd, EPOLLIN);
+    // stage 7 Replace old xps_loop_attach with modified one
+
+     xps_loop_attach(core->loop, sock_fd, EPOLLIN, listener, listener_connection_handler);
 
     // Add listener to global listeners list
-    vec_push(&listeners, listener);
+    vec_push(&core->listeners, listener);
 
     logger(LOG_DEBUG, "xps_listener_create()", "created listener on port %d", port);
 
@@ -77,13 +83,14 @@ void xps_listener_destroy(xps_listener_t *listener) {
     assert(listener != NULL);
 
     // Detach listener from loop
-    xps_loop_detach(listener->epoll_fd, listener->sock_fd);
+    xps_loop_detach(listener->core->loop, listener->sock_fd);
 
     // Set listener to NULL in 'listeners' list
-    for (int i = 0; i < listeners.length; i++) {
-        xps_listener_t *curr = listeners.data[i];
+    xps_core_t * core = listener->core;
+    for (int i = 0; i < core->listeners.length; i++) {
+        xps_listener_t *curr = core->listeners.data[i];
         if (curr == listener) {
-            listeners.data[i] = NULL;
+            core->listeners.data[i] = NULL;
         break;
         }
     }
@@ -97,8 +104,9 @@ void xps_listener_destroy(xps_listener_t *listener) {
     free(listener);
 }
 
-void xps_listener_connection_handler(xps_listener_t *listener) {
-    assert(listener != NULL);
+void listener_connection_handler(void *ptr) {
+    assert(ptr != NULL);
+    xps_listener_t *listener = ptr;
 
     struct sockaddr conn_addr;
     socklen_t conn_addr_len = sizeof(conn_addr);
@@ -112,7 +120,7 @@ void xps_listener_connection_handler(xps_listener_t *listener) {
     }
 
     // Creating connection instance
-    xps_connection_t *client = xps_connection_create(listener->epoll_fd, conn_sock_fd); // Will be implemented later
+    xps_connection_t *client = xps_connection_create(listener->core, conn_sock_fd); // Will be implemented later
     if (client == NULL) {
         logger(LOG_ERROR, "xps_listener_connection_handler()", "xps_connection_create() failed");
         close(conn_sock_fd);
